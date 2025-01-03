d745428f070ba08c7e60cb145e66e17b061e8897
Chandan Achary | IITK 27 <achandan23@iitk.ac.in>
2025-01-03 20:34:48 +0530

    Assignment 1 code

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(BASE_DIR, "data")
+PROBLEM_DIR = os.path.join(DATA_DIR, "problems")
+METADATA_DIR = os.path.join(DATA_DIR, "metadata")
+EDITORIAL_DIR = os.path.join(DATA_DIR, "editorials")
